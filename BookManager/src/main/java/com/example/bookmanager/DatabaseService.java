e25eaedc097c11c9cd49bea2553c9a32d1bc7761
Icaka1337 <ivanchev.net@abv.bg>
2024-05-25 17:56:57 +0300

    Updated DatabaseService.

--- BookManager/src/main/java/com/example/bookmanager/DatabaseService.java
+++ BookManager/src/main/java/com/example/bookmanager/DatabaseService.java
@@ -5,7 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseService {
-    private static final String CONNECTION_STRING = "jdbc:sqlserver://localhost;databaseName=Library;user=SA;password=Icaka-1337;trustServerCertificate=true;integratedSecurity=false;";
+    private static String host= "CHANGE_ME";
+    private static String dbName = "CHANGE_ME";
+    private static String user = "CHANGE_ME";
+    private static String password = "CHANGE_ME";
+
+    private static final String CONNECTION_STRING = String.format("jdbc:sqlserver://%s;databaseName=%s;user=%s;password=%s;trustServerCertificate=true;integratedSecurity=false;", host, dbName, user, password);
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(CONNECTION_STRING);
